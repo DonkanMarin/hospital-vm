13124d467d2840bb78fe262ad28c300147dd95f2
Donkan <don.marin@duocuc.cl>
2025-04-26 12:57:28 -0400

    service paciente

---
+++ src/main/java/com/example/hospital_vm/service/PacienteService.java
@@ -0,0 +1,28 @@
+package com.example.hospital_vm.service;
+
+import com.example.hospital_vm.model.Paciente;
+import com.example.hospital_vm.repository.PacienteRepository;
+
+import jakarta.persistence.Id;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PacienteService {
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public List<Paciente> findALL(){
+        return pacienteRepository.findAll();
+    }
+
+    public Optional<Paciente> getPatientById(int id){
+        return pacienteRepository.findById(id);
+    }
+
+}
